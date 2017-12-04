db115376c4cc9186956714a97824666325a10644
lamburger3 <lamburger3@gmail.com>
2017-12-03 18:22:22 -0800

    Now has code for user input and will not end until user ends the program.

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,12 +1,16 @@
 import random
 
 def rock_paper_scissors():
-    result = 0
+    result1 = raw_input("Press Enter to play Rock-Paper-Scissors.")
+    if result1 == result1:
+        result = 0
     result = result + random.randint(1,3)
     if result == 1:
         print("Your hand is Rock")
     elif result == 2:
         print("Your hand is Paper")
     else:
-        print("Your hnad is Scissors")
+        print("Your hand is Scissors")
+    rock_paper_scissors()
 rock_paper_scissors()
+
